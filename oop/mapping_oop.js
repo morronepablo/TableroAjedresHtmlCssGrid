568ad18ec8d9c91af537c7e2a9d1438c6f38c01a
Pablo Morrone <pmorrone@it.nortia.com.ar>
2022-07-18 11:57:58 -0300

    Part-10 Apply OOP Concept

---
+++ oop/mapping_oop.js
@@ -0,0 +1,9 @@
+// chess square
+// Property -> 2 Colors (color == whithe ? 0 : 1)(white = 0 && black = 1)
+// Property -> 2 isHighlighted (true or false)
+// Property -> 2 isHint (true or false)
+// Property -> 2 isPiece (true or false)
+//
+//
+//
+// Property -> 2 id (string)

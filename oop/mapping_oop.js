f9bd9d20f362c1aaf5a995874f54c432d82fbb83
Pablo Morrone <pmorrone@it.nortia.com.ar>
2022-07-27 12:32:23 -0300

    Create Chess game using HTML, CSS & Javascript! Tutorial in hindi Part-11 Create Objects

--- oop/mapping_oop.js
+++ oop/mapping_oop.js
@@ -1,5 +1,5 @@
 // chess square
-// Property -> 2 Colors (color == whithe ? 0 : 1)(white = 0 && black = 1)
+// Property -> 2 Colors (color == EEEED2 ? 0 : 1)(white => EEEED2 && black=> 769656 = 1)
 // Property -> 2 isHighlighted (true or false)
 // Property -> 2 isHint (true or false)
 // Property -> 2 isPiece (true or false)
@@ -7,3 +7,40 @@
 //
 //
 // Property -> 2 id (string)
+
+const mainMap = [];
+class Square {
+    color;
+    isHighlighted;
+    isHint;
+    isPiece;
+    id;
+}
+
+for (let i = 8; i > 0; i--) {
+    const rowArray = [];
+    for (let j = 97; j < 105; j++) {
+        const fileName = String.fromCharCode(j);
+        const id = fileName + i;
+
+        // create object of squares
+        const square = new Square();
+
+        // set Properties
+        square.id = id;
+
+        // Color
+        // odd row => odd event (769656)
+        // even row => odd event (EEEED2)
+        // odd row => even event 
+        // even row => odd event (EEEED2)
+        // even row => even event (769656)
+
+
+        // push array
+        rowArray.push(square);
+    }
+    mainMap.push(rowArray);
+}
+
+console.log(mainMap);
